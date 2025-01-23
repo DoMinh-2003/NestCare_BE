897dcdb90196b7647232e04fe1cb2ff3d8dd31f0
DoMinh-2003 <minhlola28@gmail.com>
2025-01-23 20:29:14 +0700

    setup firebase and cors

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerConfig } from './config/swagger.config';
+import { initializeFirebaseAdmin } from './config/firebase.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-  const config = new DocumentBuilder()
-    .setTitle('Nest Care')
-    .setDescription('API for managing users and authentication')
-    .setVersion('1.0') // Thêm hỗ trợ Bearer token
-    .addBearerAuth()
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-
+  app.enableCors();
+  SwaggerConfig.setupSwagger(app);
+  initializeFirebaseAdmin(app.get(ConfigService));
 
   await app.listen(process.env.PORT ?? 3000);
 }
