ed5e399e9d1fe96ba86a4fc35bafc166018fe96b
DoMinh-2003 <minhlola28@gmail.com>
2025-03-05 19:59:08 +0700

    Style/Minh Update DTO, Entity Service

--- src/services/dto/update.dto.ts
+++ src/services/dto/update.dto.ts
@@ -1,37 +1,41 @@
-import { IsDate, IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export default class UpdateServiceDto {
     constructor(
         name: string,
         description: string = '',
-        createdAt: Date = new Date(),
-        updatedAt: Date = new Date(),
-        isDeleted: number = 0,
         price: number = 0,
     ) {
         this.name = name;
         this.description = description;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.isDeleted = isDeleted;
+      
         this.price = price;
     }
 
-    @IsNotEmpty()
-    @IsString()
-    public name: string;
+    @ApiProperty({
+        description: 'The name of the product',
+        example: 'Product Name',
+      })
+      @IsNotEmpty()
+      @IsString()
+      name: string;
     
-    @IsNumber()
-    public price: number;
-
-    public description: string;
-
-    @IsDate()
-    public createdAt: Date;
-
-    @IsDate()
-    public updatedAt: Date;
-
-    @IsInt()
-    public isDeleted: number;
+      @ApiProperty({
+        description: 'The description of the product',
+        example: 'Product Description',
+      })
+      @IsNotEmpty()
+      @IsString()
+      description: string;
+    
+      @ApiProperty({
+        description: 'The price of the product',
+        example: 100,
+      })
+      @IsNotEmpty()
+      @IsInt()
+      price: number;
+    
+  
 }
