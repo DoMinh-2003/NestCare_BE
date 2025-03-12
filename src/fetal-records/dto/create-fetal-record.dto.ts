0ec791bc7d51de81b3c9d2ae4c96aefb841ae304
DoMinh-2003 <minhlola28@gmail.com>
2025-03-12 13:31:38 +0700

    Feat/Minh API Fetal Record

---
+++ src/fetal-records/dto/create-fetal-record.dto.ts
@@ -0,0 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEnum,
+  IsDate,
+  IsUUID,
+  IsDateString,
+  IsOptional,
+} from 'class-validator';
+import { PregnancyStatus } from '../entities/fetal-record.entity';
+
+export class CreateFetalRecordDto {
+  @ApiProperty({
+    description: 'The name of the fetal record',
+    example: 'Baby 1',
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: 'Ngày bắt đầu mang thai của mẹ bầu',
+    example: '2025-03-01',
+  })
+  @IsOptional()
+  @IsDateString()
+  dateOfPregnancyStart?: string; // Ngày bắt đầu mang thai
+
+  @ApiProperty({
+    description: 'Ngày dự kiến sinh của thai nhi',
+    example: '2025-12-01',
+  })
+  @IsOptional()
+  @IsDateString()
+  expectedDeliveryDate?: string; // Ngày dự kiến sinh
+
+  @ApiProperty({
+    description: 'Health status of the fetus',
+    example: 'Healthy',
+  })
+  @IsOptional()
+  @IsString()
+  healthStatus: string;
+
+  @ApiProperty({
+    description: 'Pregnancy status of the fetus',
+    enum: PregnancyStatus,
+    example: PregnancyStatus.PREGNANT,
+  })
+  @IsEnum(PregnancyStatus)
+  status: PregnancyStatus;
+
+  @ApiProperty({
+    description: 'The ID of the mother (User ID)',
+    example: 'mother-user-id',
+  })
+  @IsUUID()
+  motherId: string;
+}
---
+++ src/fetal-records/fetal-records.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { FetalRecordsService } from './fetal-records.service';
+import { FetalRecordsController } from './fetal-records.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FetalRecord } from './entities/fetal-record.entity';
+import { User } from 'src/users/model/user.entity';
+
+@Module({
+   imports: [TypeOrmModule.forFeature([FetalRecord,User])],
+  controllers: [FetalRecordsController],
+  providers: [FetalRecordsService],
+})
+export class FetalRecordsModule {}
