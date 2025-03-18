c3dfcd955324ff130e689975caad547b2556b82d
DoMinh-2003 <minhlola28@gmail.com>
2025-03-18 19:25:18 +0700

    Feat/Minh open comment blog

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,13 +1,13 @@
-// import { Module } from '@nestjs/common';
-// import { BlogsService } from './blog.service';
-// import { BlogsController } from './blog.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Blog } from './blog.entity';
+import { Module } from '@nestjs/common';
+import { BlogsService } from './blog.service';
+import { BlogsController } from './blog.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blog } from './blog.entity';
 
-// @Module({
-//   imports: [TypeOrmModule.forFeature([Blog])],
-//   providers: [BlogsService],
-//   controllers: [BlogsController],
-//   exports: [BlogsService],
-// })
-// export class BlogsModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Blog])],
+  providers: [BlogsService],
+  controllers: [BlogsController],
+  exports: [BlogsService],
+})
+export class BlogsModule {}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,127 +1,127 @@
-// import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Not, Repository } from 'typeorm';
-// import { Blog } from './blog.entity';
-// import { SearchPaginationResponseModel } from 'src/common/models';
-// import { formatPaginationResult, isEmptyObject } from 'src/utils/helpers';
-// import {
-//   CreateBlogDto,
-//   SearchBlogDto,
-//   SearchWithPaginationDto,
-//   UpdateBlogDto,
-// } from './dto';
-// import { CustomHttpException } from 'src/common/exceptions';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository } from 'typeorm';
+import { Blog } from './blog.entity';
+import { SearchPaginationResponseModel } from 'src/common/models';
+import { formatPaginationResult, isEmptyObject } from 'src/utils/helpers';
+import {
+  CreateBlogDto,
+  SearchBlogDto,
+  SearchWithPaginationDto,
+  UpdateBlogDto,
+} from './dto';
+import { CustomHttpException } from 'src/common/exceptions';
 
-// @Injectable()
-// export class BlogsService {
-//   constructor(
-//     @InjectRepository(Blog)
-//     private readonly blogRepository: Repository<Blog>,
-//   ) {}
+@Injectable()
+export class BlogsService {
+  constructor(
+    @InjectRepository(Blog)
+    private readonly blogRepository: Repository<Blog>,
+  ) {}
 
-//   async createBlog(model: CreateBlogDto, user): Promise<Blog> {
-//     if(!model){
-//         throw new CustomHttpException(HttpStatus.NOT_FOUND, 'You need to send data');
-//     }
-//     if (isEmptyObject(model)) {
-//         throw new CustomHttpException(HttpStatus.NOT_FOUND, 'Model data is empty');
-//       }
+  async createBlog(model: CreateBlogDto, user): Promise<Blog> {
+    if(!model){
+        throw new CustomHttpException(HttpStatus.NOT_FOUND, 'You need to send data');
+    }
+    if (isEmptyObject(model)) {
+        throw new CustomHttpException(HttpStatus.NOT_FOUND, 'Model data is empty');
+      }
   
 
-//     const existingBlog = await this.blogRepository.findOne({
-//       where: { title: model.title },
-//     });
-//     if (existingBlog) {
-//       throw new CustomHttpException(
-//         HttpStatus.CONFLICT,
-//         `A blog with this title: "${model.title}" already exists`,
-//       );
-//     }
-//     const newBlog = this.blogRepository.create({
-//       ...model,
-//     });
-//     return await this.blogRepository.save(newBlog);
-//   }
+    const existingBlog = await this.blogRepository.findOne({
+      where: { title: model.title },
+    });
+    if (existingBlog) {
+      throw new CustomHttpException(
+        HttpStatus.CONFLICT,
+        `A blog with this title: "${model.title}" already exists`,
+      );
+    }
+    const newBlog = this.blogRepository.create({
+      ...model,
+    });
+    return await this.blogRepository.save(newBlog);
+  }
 
-//   async getBlogs(
-//     model: SearchWithPaginationDto,
-//   ): Promise<SearchPaginationResponseModel<Blog>> {
-//     const searchCondition = {
-//       ...new SearchBlogDto(),
-//       ...model.searchCondition,
-//     };
-//     console.log(searchCondition);
-//     const { categoryId, isPublished } = searchCondition;
-//     const { pageNum, pageSize } = model.pageInfo;
-//     const query = this.blogRepository.createQueryBuilder('blog');
+  async getBlogs(
+    model: SearchWithPaginationDto,
+  ): Promise<SearchPaginationResponseModel<Blog>> {
+    const searchCondition = {
+      ...new SearchBlogDto(),
+      ...model.searchCondition,
+    };
+    console.log(searchCondition);
+    const { categoryId, isPublished } = searchCondition;
+    const { pageNum, pageSize } = model.pageInfo;
+    const query = this.blogRepository.createQueryBuilder('blog');
 
-//     // if (categoryId) {
-//     //   query.andWhere('blog.categoryId = :categoryId', { categoryId });
-//     // }
+    // if (categoryId) {
+    //   query.andWhere('blog.categoryId = :categoryId', { categoryId });
+    // }
 
-//       query.andWhere('blog.isPublished = :isPublished', {
-//         isPublished,
-//       });
+      query.andWhere('blog.isPublished = :isPublished', {
+        isPublished,
+      });
 
-//       query.orderBy('blog.createdAt', 'DESC');
+      query.orderBy('blog.createdAt', 'DESC');
 
-//     query.skip((pageNum - 1) * pageSize).take(pageSize);
+    query.skip((pageNum - 1) * pageSize).take(pageSize);
 
-//     const [blogs, total] = await query.getManyAndCount();
-//     const data = new SearchPaginationResponseModel<Blog>();
-//     const result = formatPaginationResult<Blog>(data, blogs, {
-//       pageNum,
-//       pageSize,
-//       totalItems: total,
-//       totalPages: 0,
-//     });
-//     return result;
-//   }
+    const [blogs, total] = await query.getManyAndCount();
+    const data = new SearchPaginationResponseModel<Blog>();
+    const result = formatPaginationResult<Blog>(data, blogs, {
+      pageNum,
+      pageSize,
+      totalItems: total,
+      totalPages: 0,
+    });
+    return result;
+  }
 
-//   async getBlog(id: string): Promise<Blog | null> {
-//     return await this.blogRepository.findOne({ where: { id } });
-//   }
+  async getBlog(id: string): Promise<Blog | null> {
+    return await this.blogRepository.findOne({ where: { id } });
+  }
 
-//   async updateBlog(id: string, model: UpdateBlogDto, user): Promise<Blog> {
-//     if(!model){
-//         throw new CustomHttpException(HttpStatus.NOT_FOUND, 'You need to send data');
-//     }
-//     const blog = await this.getBlog(id);
+  async updateBlog(id: string, model: UpdateBlogDto, user): Promise<Blog> {
+    if(!model){
+        throw new CustomHttpException(HttpStatus.NOT_FOUND, 'You need to send data');
+    }
+    const blog = await this.getBlog(id);
 
-//     if (!blog) {
-//       throw new CustomHttpException(
-//         HttpStatus.NOT_FOUND,
-//         `A blog with this id: "${id}" does not exist`,
-//       );
-//     }
+    if (!blog) {
+      throw new CustomHttpException(
+        HttpStatus.NOT_FOUND,
+        `A blog with this id: "${id}" does not exist`,
+      );
+    }
 
-//     if (model.title) {
-//       const existingBlog = await this.blogRepository.findOne({
-//         where: { title: model.title, id: Not(id) },
-//       });
-//       if (existingBlog) {
-//         throw new CustomHttpException(
-//           HttpStatus.BAD_REQUEST,
-//           `A blog with title "${model.title}" already exists.`,
-//         );
-//       }
-//     }
+    if (model.title) {
+      const existingBlog = await this.blogRepository.findOne({
+        where: { title: model.title, id: Not(id) },
+      });
+      if (existingBlog) {
+        throw new CustomHttpException(
+          HttpStatus.BAD_REQUEST,
+          `A blog with title "${model.title}" already exists.`,
+        );
+      }
+    }
 
-//     // Chỉ cập nhật các trường được truyền vào
-//     const updatedBlog = Object.assign(blog, model, { updatedAt: new Date() });
+    // Chỉ cập nhật các trường được truyền vào
+    const updatedBlog = Object.assign(blog, model, { updatedAt: new Date() });
 
-//     return await this.blogRepository.save(updatedBlog);
-//   }
+    return await this.blogRepository.save(updatedBlog);
+  }
 
-//   async deleteBlog(id: string): Promise<boolean> {
-//     const blog = await this.getBlog(id);
-//     if (!blog) {
-//       throw new CustomHttpException(
-//         HttpStatus.BAD_REQUEST,
-//         `A blog with this id: "${id}" not exists`,
-//       );
-//     }
-//     await this.blogRepository.update(id, { isPublished: 0 });
-//     return true;
-//   }
-// }
+  async deleteBlog(id: string): Promise<boolean> {
+    const blog = await this.getBlog(id);
+    if (!blog) {
+      throw new CustomHttpException(
+        HttpStatus.BAD_REQUEST,
+        `A blog with this id: "${id}" not exists`,
+      );
+    }
+    await this.blogRepository.update(id, { isPublished: 0 });
+    return true;
+  }
+}
