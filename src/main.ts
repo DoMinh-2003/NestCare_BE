b76c96d8fc03d77c0a652d3527408f51698d6b03
DoMinh-2003 <minhlola28@gmail.com>
2025-03-26 20:18:00 +0700

    Fix/Minh register

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerConfig } from './config/swagger.config';
 import { initializeFirebaseAdmin } from './config/firebase.config';
 import { ConfigService } from '@nestjs/config';
 import { CustomExceptionFilter } from './common/exceptions';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +14,16 @@ async function bootstrap() {
   SwaggerConfig.setupSwagger(app);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   initializeFirebaseAdmin(app.get(ConfigService));
+
+  // app.useGlobalPipes(new ValidationPipe({
+  //   exceptionFactory: (errors) => {
+  //     // Trả về lỗi chi tiết từ `class-validator`
+  //     return new BadRequestException(errors);
+  //   },
+  //   transform: true, // Chuyển đổi dữ liệu vào đúng kiểu
+  //   whitelist: true, // Tự động loại bỏ các thuộc tính không hợp lệ
+  //   forbidNonWhitelisted: true, // Từ chối các thuộc tính không có trong DTO
+  // }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
