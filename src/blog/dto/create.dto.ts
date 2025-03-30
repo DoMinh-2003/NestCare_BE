427c6db511dc5c2a59e88ea9f3f4548cdc39af76
Duc-dev-starter <leminhduck@gmail.com>
2025-03-30 11:35:41 +0700

    feat(comment): finish function to create comment

--- src/blog/dto/create.dto.ts
+++ src/blog/dto/create.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class CreateBlogDto {
   constructor(
@@ -13,19 +14,34 @@ export default class CreateBlogDto {
     this.content = content;
   }
 
+  @ApiProperty({
+    example: '663a09a9c7a5420020c1249b',
+    description: 'ID của danh mục blog',
+  })
   @IsNotEmpty()
   @IsString()
   categoryId: string;
 
-
+  @ApiProperty({
+    example: 'Top 5 mẹo chăm sóc sức khỏe mùa hè',
+    description: 'Tiêu đề bài blog',
+  })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: 'Bài viết chia sẻ những mẹo đơn giản để bảo vệ sức khỏe trong mùa hè.',
+    description: 'Mô tả ngắn về bài blog',
+  })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({
+    example: 'Nội dung chi tiết của bài viết với các mẹo như uống đủ nước, mặc đồ thoáng mát, ...',
+    description: 'Nội dung chi tiết của bài blog',
+  })
   @IsNotEmpty()
   @IsString()
   content: string;
--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('comments')
 @Controller('comments')
 export class CommentsController {
-  constructor(private readonly commentsService: CommentsService) {}
+  constructor(private readonly commentsService: CommentsService) { }
 
+
+  @ApiBearerAuth()
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
+  @ApiOperation({ summary: 'Tạo một comment mới' })
+  async createComment(@Body() createCommentDto: CreateCommentDto) {
+    const comment = await this.commentsService.createComment(createCommentDto);
+    return { success: true, data: comment };
   }
 
   @Get()
