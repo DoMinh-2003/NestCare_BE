a56b6027b09fcf4a8d8c723a1afbb36f0c5d3216
Duc-dev-starter <leminhduck@gmail.com>
2025-04-02 01:54:25 +0700

    fix(user): hide password when return data

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,12 +7,14 @@ import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { Role } from 'src/common/enums/role.enum';
 import * as admin from 'firebase-admin';
+
+import { omit } from 'lodash';
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
-  ) {}
+  ) { }
 
   async login(payload: any) {
     const user = await this.usersService.validateUser(
@@ -21,12 +23,26 @@ export class AuthService {
     );
 
     if (!user) {
-      throw new UnauthorizedException('Tài khoản hoặc mật khẩu không đúng'); // Ném lỗi 401 nếu sai thông tin
+      throw new UnauthorizedException('Tài khoản hoặc mật khẩu không đúng');
     }
 
     const token = this.jwtService.sign({ id: user.id, role: user.role });
-    return { ...user, token: token };
+
+    return { ...omit(user, ['password']), token };
   }
+  // async login(payload: any) {
+  //   const user = await this.usersService.validateUser(
+  //     payload.username,
+  //     payload.password,
+  //   );
+
+  //   if (!user) {
+  //     throw new UnauthorizedException('Tài khoản hoặc mật khẩu không đúng'); // Ném lỗi 401 nếu sai thông tin
+  //   }
+
+  //   const token = this.jwtService.sign({ id: user.id, role: user.role });
+  //   return { ...user, token: token };
+  // }
 
   async register(payload: any) {
     const existingUser = await this.usersService.findByUsername(
@@ -52,7 +68,7 @@ export class AuthService {
       if (email) {
         user = await this.usersService.findByEmail(email);
       }
-  
+
 
       if (!user) {
         const newUserPayload = {
